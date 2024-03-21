f94d7a970213b41aff08b0f656f47d88db4caaf9
Zsófi <zsofia.zsambokrety@stud.uni-corvinus.hu>
2024-03-21 13:08:37 +0100

    6. gyakorlat

---
+++ Kígyósjáték/Form1.cs
@@ -0,0 +1,96 @@
+namespace Kígyósjáték
+{
+    public partial class Form1 : Form
+    {
+        int fej_x = 100;
+        int fej_y = 100;
+        int irány_x = 1;
+        int irány_y = 0;
+        int lépésszám = 0;
+        int hossz = 20;
+        List<KígyóElem> kígyó = new List<KígyóElem>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lépésszám++;
+            fej_x += irány_x * KígyóElem.Méret;
+            fej_y += irány_y * KígyóElem.Méret;
+
+            foreach (object item in Controls)
+            {
+                if (item is KígyóElem)
+                {
+                    KígyóElem k = (KígyóElem)item;
+
+                    if (k.Top == fej_y && k.Left == fej_x)
+                    {
+                        timer1.Enabled = false;
+                        return;
+                    }
+                }
+            }
+            if (kígyó.Count > hossz)
+            {
+                KígyóElem levágandó = kígyó[0];
+                kígyó.RemoveAt(0);
+                Controls.Remove(levágandó);
+            }
+
+            KígyóElem ke = new KígyóElem();
+            ke.Top = fej_y;
+            ke.Left = fej_x;
+
+
+
+            kígyó.Add(ke);
+            Controls.Add(ke);
+
+            if (lépésszám % 2 == 0) ke.BackColor = Color.Yellow;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                irány_y = -1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                irány_y = 1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                irány_y = 0;
+                irány_x = -1;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                irány_y = 0;
+                irány_x = 1;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Méreg m=new Méreg();
+            Random random = new Random();
+            int i = random.Next(300);
+            int j = random.Next(300);
+            m.Left = i;
+            m.Top = j;
+
+        }
+    }
+}
